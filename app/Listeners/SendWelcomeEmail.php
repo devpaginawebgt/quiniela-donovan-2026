cd58aaf6651f4bbe17a910d589840d5f8b8de6a1
Dennis Herrera <dennishumher25@hotmail.com>
2026-04-17 17:53:46 -0600

    chore: made email send synchronous

--- app/Listeners/SendWelcomeEmail.php
+++ app/Listeners/SendWelcomeEmail.php
@@ -8,8 +8,9 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Mail;
 
-class SendWelcomeEmail implements ShouldQueue
+class SendWelcomeEmail
 {
+    // implements ShouldQueue
     /**
      * Create the event listener.
      */
@@ -24,6 +25,7 @@ class SendWelcomeEmail implements ShouldQueue
     public function handle(Registered $event): void
     {
         Mail::to($event->user->email)
-            ->queue(new WelcomeMail($event->user));
+            ->send(new WelcomeMail($event->user));
+            // ->queue(new WelcomeMail($event->user));
     }
 }
